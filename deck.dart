618447ce492be2416de659c0dad0b71a3a66efdc
Thiago Leite Luiz <tlluiz@tlluiz.com>
2019-05-17 19:46:45 -0300

    Creating the builddeck.

--- deck.dart
+++ deck.dart
@@ -1,12 +1,26 @@
 void main() {
   var deck = new Deck();
+  deck.buildMyCards();
 }
 
 class Deck {
   List<Card> cards;
+
+  Deck() {
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    for(var suit in suits) {
+      for(var rank in ranks) {
+        var card = new Card(suit, rank);
+      }
+    }
+  }
 }
 
 class Card {
   String suit;
   String rank;
+
+  Card(this.suit, this.rank);
 }
